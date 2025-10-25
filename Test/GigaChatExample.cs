e0e901c1b7df30cb65a6557779e060bb6d5578bd
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2025-10-25 16:18:09 +0300

    init commit

---
+++ Test/GigaChatExample.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+
+namespace Test;
+
+public class GigaChatExample
+{
+    public static async Task Run()
+    {
+        var builder = Kernel.CreateBuilder();
+        builder.AddMistralChatCompletion(
+            "mistral-medium-latest",
+            "IHO43sq5t332eHpjVQw5YkJxq7t4sxvQ",
+            new Uri("https://api.mistral.ai/v1")
+        );
+        var kernel = builder.Build();
+
+        ChatCompletionAgent agent =
+            new()
+            {
+                Name = "SK-Agent",
+                Instructions = "You are a helpful assistant.",
+                Kernel = kernel,
+            };
+
+        await foreach (StreamingChatMessageContent update in agent.InvokeStreamingAsync("Write a haiku about Semantic Kernel. i need 8 raws").ConfigureAwait(false))
+        {
+            Console.Write(update);
+        }
+
+        await foreach (AgentResponseItem<ChatMessageContent> response in (agent.InvokeAsync("Write a haiku about Semantic Kernel. i need 8 raws.")).ConfigureAwait(false))
+        {
+            Console.WriteLine(response.Message);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await Test.MistralExample.Run().ConfigureAwait(false);
+    }
+}
