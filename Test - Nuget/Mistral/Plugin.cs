b1e3e9f754f4b312adcf4cec913e41836551545a
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2025-10-25 17:20:00 +0300

    made mistral with nugets

---
+++ Test - Nuget/Mistral/Plugin.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Test;
+
+public class Plugin
+{
+    
+}
---
+++ Test/Mistral/Mistral.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Threading.Channels;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Test;
+
+public class MistralExample
+{
+    public static async Task Run()
+    {
+        var builder = Kernel.CreateBuilder();
+        builder.AddMistralChatCompletion(
+            "mistral-medium-latest",
+            "IHO43sq5t332eHpjVQw5YkJxq7t4sxvQ",
+            new Uri("https://api.mistral.ai/v1")
+        );
+        var kernel = builder.Build();
+
+
+
+        var chatService = kernel.GetRequiredService<IChatCompletionService>();
+        var chatMessages = new ChatHistory();
+        string? message = null;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Prompt:");
+                while (string.IsNullOrWhiteSpace(message))
+                {
+                    message = Console.ReadLine();
+                }
+
+                chatMessages.AddMessage(AuthorRole.User, message);
+
+                StringBuilder sb = new StringBuilder();
+                var chatCompletion = chatService.GetChatMessageContentsAsync(chatMessages, kernel: kernel);
+                foreach (var response in await chatCompletion.ConfigureAwait(false))
+                {
+                    Console.WriteLine(response);
+                    sb.Append(response.Content);
+                }
+
+                chatMessages.AddAssistantMessage(sb.ToString());
+                Console.WriteLine();
+                message = null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        ChatCompletionAgent agent =
+            new()
+            {
+                Name = "SK-Agent",
+                Instructions = "You are a helpful assistant.",
+                Kernel = kernel,
+            };
+
+        // await foreach (StreamingChatMessageContent update in agent.InvokeStreamingAsync("Write a haiku about Semantic Kernel. i need 8 raws").ConfigureAwait(false))
+        // {
+        //     Console.Write(update);
+        // }
+    }
+}
